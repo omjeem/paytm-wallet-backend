82ce9a17cd909c65de16f9cb6cd89410c9a16d39
Om Jee Mishra <93421142+om-jee-m@users.noreply.github.com>
2024-03-12 01:12:10 +0530

    route updation

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ app.use(express.json())
 dotenv.config({ path: "./config.env" }); 
 const PORT =  process.env.PORT || 3000
 
-app.get("/", function(req,res){
-    return res.status(200).send("Hey this is Paytm wallet backend");
+app.get("/",function(req,res){
+    res.status(200).json("Request Recieved");
 })
+
 const userRoute = require("./routes/index");
 app.use("/api/v1", userRoute);
 
